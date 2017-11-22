2600ba40b7f524129cc9ef619c63b1538ceb0c55
Administrator <Administrator@GENIIES-PC>
2017-11-22 16:37:44 +0530

    Movement Analysis Report

---
+++ Models/MovementAnalysisReport.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NibsMVC.Models
+{
+    public class MovementAnalysisReport
+    {
+        public int id { get; set; }
+        public int RawMaterialId { get; set; }
+        public string RawMaterialName { get; set; }
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+        public DateTime DateFrom { get; set; }
+        public DateTime DateTo { get; set; }
+    }
+}
