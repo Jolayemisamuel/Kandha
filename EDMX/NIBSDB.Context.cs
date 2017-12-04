88361a006b24aeda91e72bc1d63e44f132227061
Administrator <Administrator@GENIIES-PC>
2017-12-04 18:26:08 +0530

    Bill Details in Payment

--- EDMX/NIBSDB.Context.cs
+++ EDMX/NIBSDB.Context.cs
@@ -13,6 +13,9 @@ namespace NibsMVC.EDMX
     using System.Data.Entity;
     using System.Data.Entity.Core.Objects;
     using System.Data.Entity.Infrastructure;
+    //using System.Data.Objects;
+    //using System.Data.Objects.DataClasses;
+    //using System.Linq;
 
     public partial class NIBSEntities : DbContext
     {
@@ -85,15 +88,12 @@ namespace NibsMVC.EDMX
         public DbSet<tblAssignSubMenuItem> tblAssignSubMenuItems { get; set; }
         public DbSet<tblLedgerCategory> tblLedgerCategories { get; set; }
         public DbSet<tblLedgerGroup> tblLedgerGroups { get; set; }
-        public DbSet<tblTransByStock> tblTransByStocks { get; set; }
-        public DbSet<tblTransRetRateDet> tblTransRetRateDets { get; set; }
-        public DbSet<tblConsumption> tblConsumptions { get; set; }
-        public DbSet<vwStockTransaction> vwStockTransactions { get; set; }
-        public DbSet<vwStockTransactionsKitchen> vwStockTransactionsKitchens { get; set; }
         public DbSet<tblLedgerMaster> tblLedgerMasters { get; set; }
         public DbSet<Voucher_Entry_Credit> Voucher_Entry_Credit { get; set; }
         public DbSet<Voucher_Entry_Debit> Voucher_Entry_Debit { get; set; }
-        public DbSet<tblGRNStock> tblGRNStocks { get; set; }
+        public DbSet<tblTransByStock> tblTransByStocks { get; set; }
+        public DbSet<tblTransRetRateDet> tblTransRetRateDets { get; set; }
+        public DbSet<tblConsumption> tblConsumptions { get; set; }
     
         public virtual int BillReportGenerate(string orderType, string paymentType, Nullable<System.DateTime> datefrom, Nullable<System.DateTime> dateto, Nullable<int> billNo, string newsql)
         {
